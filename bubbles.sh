f56518a15c0a9e6a55f40cd4b2860b6539383e98
skruntskrunt <skruntskrunt@skruntskrunt.ca>
2014-08-24 21:52:25 -0600

    start bubble wrap

--- bubbles.sh
+++ bubbles.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-cd ~/projects/mongrel2-musical-relay
+cd ../mongrel2-musical-relay
 make config
+rm run/mongrel2.pid
 xterm -e make start &
-cd ~/projects/bubble-warp
+cd ../bubble-warp
 xterm -e perl http_bubble.pl &
+xterm -e perl 404-bubble.pl  &
 xterm -e sclang bubble.sc &
+firefox http://127.0.0.1/demos/bubble.html &
